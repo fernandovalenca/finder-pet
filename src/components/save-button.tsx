9239958056c5af46ed17ae047c60afaa2305c093
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-26 14:06:00 -0300

    feat: created save button component

---
+++ src/components/save-button.tsx
@@ -0,0 +1,31 @@
+import { HeartIcon } from "@heroicons/react/24/solid";
+import { ButtonHTMLAttributes, forwardRef } from "react";
+
+type ButtonProps = {
+  amount: number;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const ButtonSave = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ amount = 0 }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={classNames(
+          "py-2 px-3 flex gap-2 rounded-lg",
+          amount === 0
+            ? "bg-custom-gray-100 text-custom-gray-200"
+            : "bg-red-100 text-red-500"
+        )}
+      >
+        <HeartIcon className="h-6 w-6" />
+        <span>{`${amount} PETS`}</span>
+      </button>
+    );
+  }
+);
+
+export default ButtonSave;
