e8788ba47584280b196094c227b39428132251c8
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-26 14:07:46 -0300

    chore: settings next

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-Br">
       <Head />
-      <body>
+      <body className="h-screen w-screen">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
