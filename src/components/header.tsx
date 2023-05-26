b617385f53d71e24539680c0a7b27670fe4d4302
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-26 14:06:37 -0300

    feat: created header component

---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import ButtonSave from "./save-button";
+import FinderPetLogo from "./finder-pet-logo";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between pt-10 pb-16 px-4">
+      <div className="flex items-center justify-center gap-6">
+        <div className="flex items-center justify-center gap-2">
+          <Bars3Icon className="h-10 w-10" />
+          <FinderPetLogo />
+          <strong className="hidden ml-2">Finder Pet</strong>
+        </div>
+        <span className="hidden text-custom-gray-300 font-semibold text-sm">let's explore your pet!</span>
+      </div>
+      <ButtonSave amount={0} />
+    </header>
+  );
+}
