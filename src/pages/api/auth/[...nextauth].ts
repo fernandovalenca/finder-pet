b76ccca8d03b8b0469a73e01a257e580a975bacf
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-27 17:07:36 -0300

    feat: add social authentication route

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+});
