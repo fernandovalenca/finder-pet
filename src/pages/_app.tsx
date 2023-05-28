5c38d74eac1ba62f9c958f50fba1fdfbff671159
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-28 14:20:24 -0300

    feat: add react query provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import QueryProvider from "@/services/react-query";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryProvider>
   );
 }
