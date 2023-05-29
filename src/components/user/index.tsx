c9775d06fc86ce6e37c272b34a9e728703210254
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-29 14:51:26 -0300

    feat: implemented logout

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 export const User = () => {
   const { data: session } = useSession();
@@ -7,11 +7,12 @@ export const User = () => {
   return (
     <div className="flex gap-2 items-center justify-center">
       <img
+        loading="lazy"
         className="h-9 w-9 rounded-full"
         src={session?.user?.image || ""}
-        alt=""
+        alt="Foto do usuário logado"
       />
-      <button>
+      <button onClick={() => signOut()}>
         <ChevronDownIcon className="h-6 w-6" />
       </button>
     </div>
