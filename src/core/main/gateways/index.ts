1cc4a7e28b908d3236a73b17ef4471c2cdbb4fe1
Fernando Valença <fernandovalenca2013@gmail.com>
2023-05-31 13:52:31 -0300

    feat: created instances of user creation gateway and user creation use case

--- src/core/main/gateways/index.ts
+++ src/core/main/gateways/index.ts
@@ -1,6 +1,8 @@
-import RemoteUserAuthGateway from "@/core/application/gateways/remote-user-auth-gateways";
 import { httpClient } from "../protocols";
-import { RemotePhotoGateway } from "@/core/application/gateways/remote-photo-gateway";
+import RemotePhotoGateway from "@/core/application/gateways/remote-photo-gateway";
+import RemoteUserAuthGateway from "@/core/application/gateways/remote-user-auth-gateways";
+import RemoteCreateUserGateway from "@/core/application/gateways/remote-create-user-gateway";
 
-export const userAuthGateway = new RemoteUserAuthGateway(httpClient);
 export const photoGateway = new RemotePhotoGateway(httpClient);
+export const userAuthGateway = new RemoteUserAuthGateway(httpClient);
+export const createUserGateway = new RemoteCreateUserGateway(httpClient);
