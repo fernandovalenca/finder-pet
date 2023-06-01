ae96e2ab891f79a3aa8628be71cf4fb250b2aa07
Fernando Valença <fernandovalenca2013@gmail.com>
2023-06-01 08:47:21 -0300

    hotfix: image loading next and next config images

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -1,13 +1,17 @@
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 export const User = () => {
   const { data: session } = useSession();
 
   return (
     <div className="hidden lg:flex gap-2 items-center justify-center">
-      <img
-        loading="lazy"
+      <Image
+        width={500}
+        height={500}
+        priority={true}
+        quality={80}
         className="h-9 w-9 rounded-full"
         src={session?.user?.image || ""}
         alt="Foto do usuário logado"
