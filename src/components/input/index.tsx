74c7ad58cd664587616b60622bd2423cdcfa6e16
Fernando Valença <fernandovalenca2013@gmail.com>
2023-06-05 12:46:30 -0300

    feat: added dark mode to input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -7,7 +7,7 @@ type InputProps = {
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ title, className, ...rest }, ref) => {
     return (
-      <label className="text-sm font-semibold flex flex-col gap-2 text-stone-800">
+      <label className="text-sm font-semibold flex flex-col gap-2 text-stone-800 dark:text-neutral-100">
         {title}
         <input
           className={"w-full p-2 ring-1 ring-neutral-200 rounded-lg text-neutral-700 focus:outline-none active:outline-none " + className}
