526af34c6b2e6ccd72e313dd94c3ea1b6218492a
Fernando Valença <fernandovalenca2013@gmail.com>
2023-06-05 12:46:50 -0300

    feat: add dark mode to social button

--- src/components/social-button/index.tsx
+++ src/components/social-button/index.tsx
@@ -2,9 +2,15 @@ import { ButtonHTMLAttributes, forwardRef } from "react";
 import { LogoFacebook, LogoGoogle, LogoTwitter } from "./social-logo";
 
 const logos = {
-  google: () => <LogoGoogle className="h-5 w-5 fill-neutral-600" />,
-  twitter: () => <LogoTwitter className="h-5 w-5 fill-neutral-600" />,
-  facebook: () => <LogoFacebook className="h-5 w-5 fill-neutral-600" />,
+  google: () => (
+    <LogoGoogle className="h-5 w-5 fill-neutral-600 dark:fill-neutral-100" />
+  ),
+  twitter: () => (
+    <LogoTwitter className="h-5 w-5 fill-neutral-600 dark:fill-neutral-100" />
+  ),
+  facebook: () => (
+    <LogoFacebook className="h-5 w-5 fill-neutral-600 dark:fill-neutral-100" />
+  ),
 };
 
 type SocialButtonProps = {
@@ -18,13 +24,13 @@ export const SocialButton = forwardRef<HTMLButtonElement, SocialButtonProps>(
     return (
       <button
         ref={ref}
-        className={`w-full flex items-center justify-between py-2 px-3 border border-neutral-500 rounded-lg ${className}`}
+        className={`w-full flex items-center justify-between py-2 px-3 border border-neutral-500 dark:border-neutral-100 rounded-lg ${className}`}
         {...rest}
       >
         <span className="fill-neutral-500">
           <LogoSocial />
         </span>
-        <span className="flex-1 font-semibold text-sm text-neutral-600">
+        <span className="flex-1 font-semibold text-sm text-neutral-600 dark:text-neutral-100">
           {children}
         </span>
       </button>
