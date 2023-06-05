25ff9c3895d55cbfda09785764f51fadfbddcc76
Fernando Valença <fernandovalenca2013@gmail.com>
2023-06-05 12:54:57 -0300

    feat: add dark mode to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,16 +2,16 @@ export function Footer() {
   return (
     <footer className="">
       <div className="flex items-center justify-between border divide-x-2 text-center">
-        <a className="flex-1 py-8  cursor-pointer">Facebook</a>
-        <a className="flex-1 py-8 cursor-pointer">Instagram</a>
-        <a className="flex-1 py-8 cursor-pointer">Twitter</a>
+        <a className="flex-1 py-8  cursor-pointer dark:text-neutral-100">Facebook</a>
+        <a className="flex-1 py-8 cursor-pointer dark:text-neutral-100">Instagram</a>
+        <a className="flex-1 py-8 cursor-pointer dark:text-neutral-100">Twitter</a>
       </div>
-      <h2 className="w-full font-semibold text-5xl sm:text-7xl py-10 pl-6 pr-14 break-words">
+      <h2 className="w-full font-semibold text-5xl sm:text-7xl py-10 pl-6 pr-14 break-words dark:text-neutral-100">
         18,313,224<span className="text-red-400"> pets</span> for
         <br />
         you
       </h2>
-      <span className="mb-5 ml-6">© 2022 All rights reserved.</span>
+      <span className="mb-5 ml-6 dark:text-neutral-100">© 2022 All rights reserved.</span>
     </footer>
   );
 }
